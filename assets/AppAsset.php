75f7c6bf3b2a712b9b1fa4c79b4a73fb1b22d0d2
Elelion <elelion@yandex.ru>
2020-03-17 18:55:03 +0300

    including plugins

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -37,7 +37,15 @@ class AppAsset extends AssetBundle
 		'js/jquery.scrollUp.min.js',
 		'js/price-range.js',
 		'js/jquery.prettyPhoto.js',
-		'js/main.js'
+
+        /*
+         * NOTE:
+         * jquery.accordion.js - remembers the state of the accordion
+         * */
+        'js/jquery.accordion.js',
+        'js/jquery.cookie.js',
+
+        'js/main.js',
 	];
 	public $depends = [
 		'yii\web\YiiAsset',
