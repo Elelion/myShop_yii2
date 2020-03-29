482d38ec78e0dd4e3d5b32b2f78d475df123910d
Elelion <elelion@yandex.ru>
2020-03-29 16:08:12 +0300

    creating the set Meta method to output meta tags

--- controllers/AppController.php
+++ controllers/AppController.php
@@ -7,5 +7,21 @@ use yii\web\Controller;
 // NOTE: A shared controller that will be available for all other controllers
 class AppController extends Controller
 {
-
+    // NOTE: setting SEO tags
+    protected function setMeta($title = null, $keywords = null, $description = null)
+    {
+        /*
+         * NOTE:
+         * $this ->view - pass to the current view, from the controller method
+         * where setMeta is called
+         * */
+        $this->view->title = $title;
+        $this->view->registerMetaTag(['name' => 'keywords', 'content' => "$keywords"]);
+        $this->view->registerMetaTag(['name' => 'description', 'content' => "$description"]);
+    }
 }
+
+/*
+NOTE:
+registerMetaTag - set meta tags on a page
+*/
