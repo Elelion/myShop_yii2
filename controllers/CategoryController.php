96dd5e75f02bcc2162812aa09db3aa30cab6d7ee
Elelion <elelion@yandex.ru>
2020-04-09 18:52:20 +0300

    create a pagination

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -5,6 +5,7 @@ namespace app\controllers;
 use app\models\Category;
 use app\models\Product;
 use Yii;
+use yii\data\Pagination;
 
 class CategoryController extends AppController
 {
@@ -23,20 +24,47 @@ class CategoryController extends AppController
     // NOTE: http://localhost/yii2/basic_shop/web/category/4
     public function actionView($id)
     {
-        /*
-         * NOTE:
-         * getting the id from the url.
-         * This line is optional, everything will work without it
-         * */
         $id = Yii::$app->request->get('id');
 
-        $products = Product::find()
-            ->where(['category_id' => $id])
+        /**/
+
+        // NOTE: our query that returns all records
+        $query = Product::find()
+            ->where(['category_id' => $id]);
+
+        /*
+         * NOTE:
+         * calculates the total number of entries, for further pagination
+         *
+         * 'pageSize' => 3 - outputs 3 entries per page
+         * if this parameter is omitted, 20 is output by default
+         *
+         * 'forcePageParam' => false + 'pageSizeParam' => false - for clear url
+         * /web/category/4?per-page=3 -> .../4?per-page=3 - it is trash
+         *
+         * rules - look at web.php -> urlManager -> rules
+         * */
+        $pages = new Pagination([
+            'totalCount' => $query->count(),
+            'pageSize' => 3,
+            'forcePageParam' => false,
+            'pageSizeParam' => false
+        ]);
+
+        /*
+         * NOTE:
+         * $query->offset($pages->offset) - we say which entry to start with
+         * ->limit($pages->limit) - number of entries per page
+         * ->all(); - select from all entries
+         * */
+        $products = $query->offset($pages->offset)
+            ->limit($pages->limit)
             ->all();
+
+        /**/
 
         $category = Category::findOne($id);
 
-        // NOTE: setting meta tags for our View
         $this->setMeta(
             'E-SHOP__view | ' . $category->name,
             $category->keywords,
@@ -45,6 +73,7 @@ class CategoryController extends AppController
 
         return $this->render('view', compact(
             'products',
+            'pages',
             'category'
         ));
     }
@@ -52,10 +81,6 @@ class CategoryController extends AppController
 
 /*
 NOTE:
-http://localhost/yii2/basic_shop/web/category/4
-does not output keywords & description,
-because they are not registered in the database under id 4
-
-http://localhost/yii2/basic_shop/web/category/29
-outputs keywords & description
+an example of pagination is given here:
+https://www.yiiframework.com/doc/guide/2.0/ru/output-pagination
 */
