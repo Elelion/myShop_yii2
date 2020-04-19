c247fa9458b0c16e915ff5fa71153e413280c763
elelion <elelion@Yandex.ru>
2020-04-19 23:38:35 +0300

    fix errors

--- views/category/view.php
+++ views/category/view.php
@@ -51,7 +51,7 @@ use yii\widgets\LinkPager;
                     </div><!--/price-range-->
 
                     <div class="shipping text-center"><!--shipping-->
-                        img src="/images/home/shipping.jpg" alt="" />
+                        <img src="/images/home/shipping.jpg" alt="" />
                     </div><!--/shipping-->
 
                 </div>
