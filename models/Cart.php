51549e40958c60d45bb0a59fc2a297d944fe8cbd
elelion <elelion@Yandex.ru>
2020-05-11 12:07:12 +0300

    fix errors for sum

--- models/Cart.php
+++ models/Cart.php
@@ -32,7 +32,7 @@ class Cart extends ActiveRecord
             ? $_SESSION['cart.qty'] + $qty
             : $qty;
 
-        $_SESSION['cart.qty'] = isset($_SESSION['cart.sum'])
+        $_SESSION['cart.sum'] = isset($_SESSION['cart.sum'])
             ? $_SESSION['cart.sum'] + $qty * $product->price
             : $qty * $product->price;
     }
