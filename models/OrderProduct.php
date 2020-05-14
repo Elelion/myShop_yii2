11e2986a620973d6baacda6eab87a64ca4fcda1b
elelion <elelion@Yandex.ru>
2020-05-14 08:45:40 +0300

    create a behaviors

--- models/OrderProduct.php
+++ models/OrderProduct.php
@@ -5,6 +5,7 @@ namespace app\models;
 use Yii;
 use yii\db\ActiveRecord;
 use yii\behaviors\TimestampBehavior;
+use yii\db\Expression;
 
 /**
  * This is the model class for table "order_product".
@@ -28,6 +29,22 @@ class OrderProduct extends ActiveRecord
     public static function tableName()
     {
         return 'order_product';
+    }
+
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => TimestampBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+                ],
+
+                // если вместо метки времени UNIX используется datetime:
+                 'value' => new Expression('NOW()'),
+            ],
+        ];
     }
 
     public function getOrderItems()
